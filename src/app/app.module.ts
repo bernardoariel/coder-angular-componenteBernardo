f50a4531c39583ffe7ce6d7e396e971e1caabe43
Ariel Bernardo <64559228+bernardoariel@users.noreply.github.com>
2023-03-16 14:41:26 -0300

    ComponenteBernardo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HomeComponent } from './home/home.component';
+import { PageWrapperComponent } from './page-wrapper/page-wrapper.component';
+import { BreadcrumComponent } from './breadcrum/breadcrum.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    SidebarComponent,
+    HomeComponent,
+    PageWrapperComponent,
+    BreadcrumComponent,
+
   ],
   imports: [
     BrowserModule
---
+++ src/app/breadcrum/breadcrum.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-breadcrum',
+  templateUrl: './breadcrum.component.html',
+  styles: [
+  ]
+})
+export class BreadcrumComponent {
+
+}
